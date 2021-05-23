3937aed67525a8b17b3251e0e60490e203baff98
Kapirka <68941822+KapirkaZ@users.noreply.github.com>
2021-05-23 16:01:30 +0300

    netlify page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, Suspense, lazy } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import routes from './routes';
+import Container from './components/Container';
+import AppBar from './components/AppBar';
+import Loader from './components/Loader';
+import { authOperations } from './redux/auth';
+import { connect } from 'react-redux';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 
-function App() {
+const HomePage = lazy(() =>
+  import('./views/HomePage' /* webpackChunkName: "home-page" */),
+);
+
+const RegisterPage = lazy(() =>
+  import('./views/RegisterPage' /* webpackChunkName: "register-page" */),
+);
+const LoginPage = lazy(() =>
+  import('./views/LoginPage' /* webpackChunkName: "login-page" */),
+);
+const ContactsPage = lazy(() =>
+  import('./views/ContactsPage' /* webpackChunkName: "contacts-page" */),
+);
+
+const App = ({ onGetCurrentUser }) => {
+  useEffect(() => {
+    onGetCurrentUser();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppBar />
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route exact path={routes.home} component={HomePage} />
+          <PublicRoute
+            path={routes.register}
+            restricted
+            component={RegisterPage}
+            redirectTo={routes.contacts}
+          />
+          <PublicRoute
+            path={routes.login}
+            restricted
+            component={LoginPage}
+            redirectTo={routes.contacts}
+          />
+          <PrivateRoute
+            path={routes.contacts}
+            component={ContactsPage}
+            redirectTo={routes.login}
+          />
+          <Redirect to={routes.home} />
+        </Switch>
+      </Suspense>
+    </Container>
   );
-}
+};
 
-export default App;
+const mapDispatchToProps = {
+  onGetCurrentUser: authOperations.getCurrentUser,
+};
+
+export default connect(null, mapDispatchToProps)(App);
---
+++ src/views/HomePage.js
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <h1 className="homapage-title">Welcome to Phonebook Application</h1>;
+};
+
+export default HomePage;
